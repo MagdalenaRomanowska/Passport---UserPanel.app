a580c815241672215be327e5671c38fe55293376
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2020-11-07 19:36:19 +0100

    Submodule 31.4 new endpoints, task 1 of 3

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 router.get('/logged', (req, res) => {
-  res.render('logged');
+    if (req.user !== undefined) {
+        res.render('logged');
+    } else {
+        res.render('noPermission');
+    }
+    console.log('req.user:', req.user);
 });
 
 router.get('/no-permission', (req, res) => {
-  res.render('noPermission');
+    res.render('noPermission');
+});
+
+router.get('/profile', (req, res) => {
+    if (req.user !== undefined) {
+        res.render('profile');
+    } else {
+        res.render('noPermission');
+    }
+    console.log('req.user:', req.user);
+});
+
+router.get('/profile/settings', (req, res) => {
+    if (req.user !== undefined) {
+        res.render('profileSettings');
+    } else {
+        res.render('noPermission');
+    }
+    console.log('req.user:', req.user);
 });
 
 module.exports = router;
